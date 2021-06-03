f7dffd9f5f4083285f550b803fda41c095967839
Sofiia Artemchuk <sofi.artemchuk@gmail.com>
2021-06-03 09:16:15 +0300

    money per day loop

--- task_1/script.js
+++ task_1/script.js
@@ -1,10 +1,8 @@
-"use strict";
-let money = +prompt("Ваш бюджет на месяц?", "12000");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "2021-05-27");
-let firstSpending = prompt("Введите обязательную статью расходов в этом месяце");
-let firstCosts = +prompt("Во сколько обойдется?");
-let secondSpending = prompt("Введите обязательную статью расходов в этом месяце");
-let secondCosts = +prompt("Во сколько обойдется?");
+'use strict';
+
+let money = +prompt("Ваш бюджет на місяць?", "12000");
+let time = prompt("Введіть дату в форматі YYYY-MM-DD", "2021-05-27");
+
 
 let appData = {
     budget: money,
@@ -13,8 +11,33 @@ let appData = {
     optionalExpenses: {},
     income: [],
     savings: false
-}
-appData.expenses.firstSpending = firstCosts;
-appData.expenses.secondSpending = secondCosts;
+};
 
-alert("Daily budget: " + appData.budget / 30);
+
+
+for (let i=0; i< 2; i++) {
+    let Spending = prompt("Введіть обов'язкову статтю розходів в цьому місяці");
+    let Costs = prompt("В скільки обійдеться?");
+
+    if( typeof(Spending) ==='string' && typeof(Spending) != null && typeof(Costs) != null && Spending != '' && Costs != '' && Spending.length < 50) {
+        console.log ("done");
+        appData.expenses[Spending]= Costs;
+    } else {
+        alert ("Поганий результат. Спробуйте ще раз");
+        i--;
+    }
+}
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Денний бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100){
+    console.log ("мінімальний рівень достатку");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log ("середній рівень достатку");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay > 2000) {
+    console.log ("високий рівень достатку");
+} else {
+    console.log ("відбулась помилка");
+}
